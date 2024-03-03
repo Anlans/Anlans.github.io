038703ac3c5666654dfe1385888c166599fd5991
qnq <shiaqiang2@gmail.com>
2024-03-03 11:09:50 +0800

    delete some don't use module for now

--- .vitepress/config/sidebar.ts
+++ .vitepress/config/sidebar.ts
@@ -27,131 +27,131 @@ export const sidebar = {
       ]
     },
   ],
-  '/article/': [
-    {
-      text: '里程碑',
-      items: [
-        { text: '里程碑 2021', link: '/article/milestone/2021' },
-        { text: '里程碑 2022', link: '/article/milestone/2022' },
-        { text: '里程碑 2024', link: '/article/milestone/2024' },
-      ]
-    },
-    {
-      text: '年度总结',
-      items: [
-        { text: '2022 年度总结', link: '/article/annual-summary/2022' },
-        { text: '2023 年度总结', link: '/article/annual-summary/2023' },
-      ]
-    },
-    {
-      text: '我的文章',
-      items: [
-        { text: '记事本', link: '/article/article/1' },
-        { text: '关于组件库', link: '/article/article/2' },
-        { text: '看书的分享', link: '/article/article/3' },
-        { text: 'GitHubPages 部署项目', link: '/article/article/4' },
-        { text: 'highlightjs 使用方法', link: '/article/article/5' },
-        { text: 'Node.js 版本切换', link: '/article/article/6' },
-        {
-          text: 'Vue3 + pnpm 搭建 monorepo 项目',
-          link: '/article/article/7'
-        },
-        { text: 'Git 基础知识', link: '/article/article/8' },
-        { text: 'Vue3 的源码怎么看', link: '/article/article/9' },
-        {
-          text: 'Vite + TS 配置 .env 环境文件',
-          link: '/article/article/10'
-        },
-        {
-          text: 'Vue3 开源组件库 Fighting Design 成员招募',
-          link: '/article/article/11'
-        },
-        {
-          text: '想参与开源贡献吗？Fighting Design 欢迎加入！',
-          link: '/article/article/12'
-        },
-        {
-          text: 'Github 如何提交 PR？',
-          link: '/article/article/13'
-        },
-        {
-          text: '我是如何学习的？',
-          link: '/article/article/14'
-        },
-        {
-          text: '分享一些我的开源项目',
-          link: '/article/article/15'
-        },
-        {
-          text: '关于图片加载，你需要学习一下',
-          link: '/article/article/16'
-        },
-        {
-          text: '远程计算机或设备将不接受链接-解决方案',
-          link: '/article/article/17'
-        },
-        {
-          text: 'Linux 初级自动化打包部署',
-          link: '/article/article/18'
-        },
-        {
-          text: 'Vue3 组件库 Fighting Design 欢迎加入',
-          link: '/article/article/19'
-        },
-        {
-          text: '纯原生开发 Web Components 超详细分享',
-          link: '/article/article/20'
-        },
-        { text: '别再写循环套循环了', link: '/article/article/21' },
-      ]
-    },
-    {
-      text: 'JavaScript 设计模式',
-      items: [
-        { text: '基础知识', link: '/article/design-mode/1' },
-        { text: '高阶函数', link: '/article/design-mode/2' },
-        { text: '单例模式', link: '/article/design-mode/3' },
-        { text: '策略模式', link: '/article/design-mode/4' },
-        { text: '代理模式', link: '/article/design-mode/5' },
-        { text: '迭代器模式', link: '/article/design-mode/6' },
-        { text: '发布订阅模式', link: '/article/design-mode/7' },
-        { text: '命令模式', link: '/article/design-mode/8' },
-        { text: '组合模式', link: '/article/design-mode/9' },
-        { text: '模板方法模式', link: '/article/design-mode/10' },
-        { text: '享元模式', link: '/article/design-mode/11' },
-        { text: '职责链模式', link: '/article/design-mode/12' },
-        { text: '状态模式', link: '/article/design-mode/13' },
-        { text: '适配器模式', link: '/article/design-mode/14' }
-      ]
-    },
-    {
-      text: 'Vue.js 设计与实现',
-      items: [
-        { text: '第一章 权衡的艺术', link: '/article/vue-design/1' },
-        {
-          text: '第二章 框架设计的核心要素',
-          link: '/article/vue-design/2'
-        },
-        {
-          text: '第三章 Vue.js 3 的设计思路',
-          link: '/article/vue-design/3'
-        },
-        {
-          text: '第四章 响应系统的作用与实现',
-          link: '/article/vue-design/4'
-        }
-      ]
-    },
-    {
-      text: '前端面试题',
-      items: [
-        { text: '更新面试题', link: '/article/ques/1' },
-        { text: 'Web 相关', link: '/article/ques/2' },
-        { text: 'Html 相关', link: '/article/ques/3' },
-        { text: 'Css 相关', link: '/article/ques/4' },
-        { text: 'JavaScript 相关', link: '/article/ques/5' },
-        { text: 'Vue2 相关', link: '/article/ques/6' }
-      ]
-    }
-  ]
+  // '/article/': [
+  //   {
+  //     text: '里程碑',
+  //     items: [
+  //       { text: '里程碑 2021', link: '/article/milestone/2021' },
+  //       { text: '里程碑 2022', link: '/article/milestone/2022' },
+  //       { text: '里程碑 2024', link: '/article/milestone/2024' },
+  //     ]
+  //   },
+  //   {
+  //     text: '年度总结',
+  //     items: [
+  //       { text: '2022 年度总结', link: '/article/annual-summary/2022' },
+  //       { text: '2023 年度总结', link: '/article/annual-summary/2023' },
+  //     ]
+  //   },
+  //   {
+  //     text: '我的文章',
+  //     items: [
+  //       { text: '记事本', link: '/article/article/1' },
+  //       { text: '关于组件库', link: '/article/article/2' },
+  //       { text: '看书的分享', link: '/article/article/3' },
+  //       { text: 'GitHubPages 部署项目', link: '/article/article/4' },
+  //       { text: 'highlightjs 使用方法', link: '/article/article/5' },
+  //       { text: 'Node.js 版本切换', link: '/article/article/6' },
+  //       {
+  //         text: 'Vue3 + pnpm 搭建 monorepo 项目',
+  //         link: '/article/article/7'
+  //       },
+  //       { text: 'Git 基础知识', link: '/article/article/8' },
+  //       { text: 'Vue3 的源码怎么看', link: '/article/article/9' },
+  //       {
+  //         text: 'Vite + TS 配置 .env 环境文件',
+  //         link: '/article/article/10'
+  //       },
+  //       {
+  //         text: 'Vue3 开源组件库 Fighting Design 成员招募',
+  //         link: '/article/article/11'
+  //       },
+  //       {
+  //         text: '想参与开源贡献吗？Fighting Design 欢迎加入！',
+  //         link: '/article/article/12'
+  //       },
+  //       {
+  //         text: 'Github 如何提交 PR？',
+  //         link: '/article/article/13'
+  //       },
+  //       {
+  //         text: '我是如何学习的？',
+  //         link: '/article/article/14'
+  //       },
+  //       {
+  //         text: '分享一些我的开源项目',
+  //         link: '/article/article/15'
+  //       },
+  //       {
+  //         text: '关于图片加载，你需要学习一下',
+  //         link: '/article/article/16'
+  //       },
+  //       {
+  //         text: '远程计算机或设备将不接受链接-解决方案',
+  //         link: '/article/article/17'
+  //       },
+  //       {
+  //         text: 'Linux 初级自动化打包部署',
+  //         link: '/article/article/18'
+  //       },
+  //       {
+  //         text: 'Vue3 组件库 Fighting Design 欢迎加入',
+  //         link: '/article/article/19'
+  //       },
+  //       {
+  //         text: '纯原生开发 Web Components 超详细分享',
+  //         link: '/article/article/20'
+  //       },
+  //       { text: '别再写循环套循环了', link: '/article/article/21' },
+  //     ]
+  //   },
+  //   {
+  //     text: 'JavaScript 设计模式',
+  //     items: [
+  //       { text: '基础知识', link: '/article/design-mode/1' },
+  //       { text: '高阶函数', link: '/article/design-mode/2' },
+  //       { text: '单例模式', link: '/article/design-mode/3' },
+  //       { text: '策略模式', link: '/article/design-mode/4' },
+  //       { text: '代理模式', link: '/article/design-mode/5' },
+  //       { text: '迭代器模式', link: '/article/design-mode/6' },
+  //       { text: '发布订阅模式', link: '/article/design-mode/7' },
+  //       { text: '命令模式', link: '/article/design-mode/8' },
+  //       { text: '组合模式', link: '/article/design-mode/9' },
+  //       { text: '模板方法模式', link: '/article/design-mode/10' },
+  //       { text: '享元模式', link: '/article/design-mode/11' },
+  //       { text: '职责链模式', link: '/article/design-mode/12' },
+  //       { text: '状态模式', link: '/article/design-mode/13' },
+  //       { text: '适配器模式', link: '/article/design-mode/14' }
+  //     ]
+  //   },
+  //   {
+  //     text: 'Vue.js 设计与实现',
+  //     items: [
+  //       { text: '第一章 权衡的艺术', link: '/article/vue-design/1' },
+  //       {
+  //         text: '第二章 框架设计的核心要素',
+  //         link: '/article/vue-design/2'
+  //       },
+  //       {
+  //         text: '第三章 Vue.js 3 的设计思路',
+  //         link: '/article/vue-design/3'
+  //       },
+  //       {
+  //         text: '第四章 响应系统的作用与实现',
+  //         link: '/article/vue-design/4'
+  //       }
+  //     ]
+  //   },
+  //   {
+  //     text: '前端面试题',
+  //     items: [
+  //       { text: '更新面试题', link: '/article/ques/1' },
+  //       { text: 'Web 相关', link: '/article/ques/2' },
+  //       { text: 'Html 相关', link: '/article/ques/3' },
+  //       { text: 'Css 相关', link: '/article/ques/4' },
+  //       { text: 'JavaScript 相关', link: '/article/ques/5' },
+  //       { text: 'Vue2 相关', link: '/article/ques/6' }
+  //     ]
+  //   }
+  // ]
 }
